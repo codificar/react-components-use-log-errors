e13d0dda03eaa4edf3a87ad6ea528bb66f8f7d4f
rafael.rehfeld <rafael.rehfeld@codificar.com.br>
2023-11-07 14:22:04 -0300

    alterando a váriavel currentLocale para settings.language

--- src/lang/strings.tsx
+++ src/lang/strings.tsx
@@ -7,7 +7,7 @@ const deviceLanguage =
 		? NativeModules.SettingsManager.settings.AppleLanguages[0]
 		: NativeModules.I18nManager.localeIdentifier;
 
-if (deviceLanguage == 'es_PY' || 'es-PY') {
+if (deviceLanguage == 'es_PY' || deviceLanguage.includes('es')) {
 	strings = require('./es-PY.json');
 }
 
